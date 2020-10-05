56cb06b0fac5b518cb7797eeb3a2adee351fe33d
Gary <gkwheeler40@gmail.com>
2020-10-04 20:34:30 -0600

    add idex and create actions. add strong params

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::MoviesController < ApplicationController
+
+    def index
+        movies = Movie.all
+        render json: movies
+    end
+
+    def create
+        movie = Movie.new(movie_params)
+        if movie.save
+            render json: movie, status: :accepted
+        else
+            render json: {errors: movie.errors.full_message}, status: :unprocessible_entity
+    end
+
+    private
+
+    def movie_params
+        params.require(:movie).permit(:title, :description, :image_url, :category_id)
+    end
 end
