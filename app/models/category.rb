59bda8c9c2e07b2c2e2050ab0abb5421138e97c5
Gary <gkwheeler40@gmail.com>
2020-10-04 18:05:07 -0600

    add associations and seed data

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ApplicationRecord
+    has_many :movies, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,8 @@ romance = Category.create(name: "romance")
 thriller = Category.create(name: "thriller")
 western = Category.create(name: "western")
 
-movie.create(title: "Star Wars: Episode V - The Empire Strikes Back", description: "After the Rebels are brutally overpowered 
+Movie.create(title: "Star Wars: Episode V - The Empire Strikes Back", description: "After the Rebels are brutally overpowered 
 by the Empire on the ice planet Hoth, Luke Skywalker begins Jedi training with Yoda, 
 while his friends are pursued by Darth Vader and a bounty hunter named Boba Fett all over the galaxy.", image_url: "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg", category_id: sci_fi.id)
 
-movie.create(title: "As Good as It Gets", description: "A single mother and waitress, a misanthropic author, and a gay artist form an unlikely friendship after the artist is assaulted in a robbery.", image_url: "https://m.media-amazon.com/images/M/MV5BNWMxZTgzMWEtMTU0Zi00NDc5LWFkZjctMzUxNDIyNzZiMmNjXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg", category_id: comedy.id)
+Movie.create(title: "As Good as It Gets", description: "A single mother and waitress, a misanthropic author, and a gay artist form an unlikely friendship after the artist is assaulted in a robbery.", image_url: "https://m.media-amazon.com/images/M/MV5BNWMxZTgzMWEtMTU0Zi00NDc5LWFkZjctMzUxNDIyNzZiMmNjXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg", category_id: comedy.id)
