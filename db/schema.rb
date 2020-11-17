f1ac28a6ae73143621bca7b471604f5c4965fe72
Gary <gkwheeler40@gmail.com>
2020-11-17 15:30:54 -0700

    create migration to add rating to movies table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_04_031907) do
+ActiveRecord::Schema.define(version: 2020_11_16_203817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_10_04_031907) do
     t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "rating", default: 0
     t.index ["category_id"], name: "index_movies_on_category_id"
   end
 
